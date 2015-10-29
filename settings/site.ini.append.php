b552b8c2e731a230cdafab729adaf4fc8708833e
Luca Realdi <luca.realdi@opencontent.it>
2015-10-29 09:33:39 +0100

    Remove [TemplateSettings]ExtensionAutoloadPath[]=ezuniquedatatypes from site.ini

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -1,7 +1,7 @@
 <?php /* #?ini charset="utf-8"?
 
 [TemplateSettings]
-ExtensionAutoloadPath[]=ezuniquedatatypes
+#ExtensionAutoloadPath[]=ezuniquedatatypes
 
 [RegionalSettings]
 TranslationExtensions[]=ezuniquedatatypes
